6a6ec1cd176a5a1621539cec2b60e128f2043f3d
Zoe Kim <zoek2100@gmail.com>
2022-02-26 14:46:48 -0500

    feat : take in user's input for prediction

--- scripts/app.py
+++ scripts/app.py
@@ -3,6 +3,10 @@ from streamlit_folium import folium_static
 import folium
 import json
 import requests
+import googlemaps
+import pandas as pd
+import numpy as np
+import datetime
 
 with open('apikey.txt') as f:
     api_key = f.readline()
@@ -13,7 +17,7 @@ def main():
     st.title('Safe-U-Map')
     pages = ['Home', 'Chart', 'Map']
     choice = st.sidebar.radio('Select Pages', pages)
-
+    
     if choice == 'Home':
         st.subheader('What is Safe-U-Map?')
 
@@ -25,15 +29,33 @@ def main():
         default_value = 'N/A'
         start = st.text_input("Where are you?", default_value)
         destination = st.text_input("Your Destination", default_value)
+        t = st.time_input('When?', datetime.time(8, 45))
+        if start is not None:
 
+            GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ start +'&key='+ api_key
+            geo_response = requests.request("GET", GEOCODE_URL)
+            geodata = json.loads(geo_response.text)
 
-        GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ start +'&key='+ api_key
-        geo_response = requests.request("GET", GEOCODE_URL)
-        geodata = json.loads(geo_response.text)
-        print(geodata)
+            if geodata['status'] != 'ZERO_RESULTS':
+                lat = geodata['results'][0]['geometry']['location']['lat']
+                long = geodata['results'][0]['geometry']['location']['lng']
         
-        m = folium.Map(location=[39.949610, -75.150282], zoom_start = 16)
-        folium_static(m)
+                m = folium.Map(location= [lat, long], zoom_start = 16)
+                folium_static(m)
+
+                gmaps = googlemaps.Client(key = api_key)
+
+                geocode_result = gmaps.geocode(start)
+                print(geodata['results'][0]['geometry']['location']['lat'])
+
+                def get_user_data():
+                    record_data = {
+                        'hour' : t.hour,
+                        'lat' : lat,
+                        'log' : log
+                    }
+                    return pd.DataFrame(record_data, index=[0])
+
 
 if __name__ == '__main__':
     main()
